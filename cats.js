a9655e87c6ab95cfcc58671570df400f7c132349
Anthematics <jasonharder2@gmail.com>
2017-10-11 19:55:49 -0400

    v0.1 can add a picture of one cat right now to

--- cats.js
+++ cats.js
@@ -4,23 +4,31 @@ document.addEventListener("DOMContentLoaded",function() {
 		buttoncats.addEventListener('click',function(e) {
 			e.preventDefault()
 			$.ajax ({
-				url: 'http://bitkittens.herokuapp.com',
+				url: 'http://bitkittens.herokuapp.com/cats.json',
 				method: 'GET' ,
 				datatype:  'JSON'
 
+			}).done(function(data){
+console.log(data.cats[0].photo)
+// Creates a new <img> tag -> var element = document.createElement('img');
+
+
+			let newcat = document.createElement("IMG"); //so we are creating a image element for the image
+
+			
+			let catbox = document.getElementById("cat1"); //so we are getting a picture of a cat
+			newcat.src = data.cats[0].photo; // so we are grabbing a picture from the api
+			newcat.alt="photo of " + data.cats[0].name; //so we are making its
+			//alt in the img descriptor show the cats name
+
+
+			catbox.appendChild(newcat);
+// Inserts that <img> into one of the empty .cat-box divs'
+
+create a for loop to go thru the cats and add
 			})
 
-// Creates a new <img> tag -> var element = document.createElement('img');
-			let newcat = document.createElement("IMG");
 
-			let visitcat = document.getElementById("imageid");
-			a.src="../template/save.png";
-			a.alt
-// Sets the src attribute of the <img> to the cat's photo
-// Sets the alt attribute of the <img> to "Photo of (insert cat name here)"
-
-			var addcat = cat-box.appendChild(newcat);
-// Inserts that <img> into one of the empty .cat-box divs'
 		})
 	})
 
