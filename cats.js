9603232464a5ccf4b5d5517aefc0a030a637d6c9
Anthematics <jasonharder2@gmail.com>
2017-10-11 20:16:47 -0400

    kittens now add to spaces

--- cats.js
+++ cats.js
@@ -1,31 +1,35 @@
-document.addEventListener("DOMContentLoaded",function() {
+document.addEventListener("DOMContentLoaded",function() { //Dom must be loaded before JS executes at all.
 
-	let buttoncats = document.querySelector('.summon-cats');
+	let buttoncats = document.querySelector('.summon-cats'); ///makes buttoncats = the literal cat button in the index file
 		buttoncats.addEventListener('click',function(e) {
-			e.preventDefault()
-			$.ajax ({
-				url: 'http://bitkittens.herokuapp.com/cats.json',
-				method: 'GET' ,
-				datatype:  'JSON'
+			e.preventDefault() //prevent something from loading so AJAX request can successfully send.
+			$.ajax ({ //ajax request
+				url: 'http://bitkittens.herokuapp.com/cats.json', //from bitkittens
+				method: 'GET' , //says it is a get request
+				datatype:  'JSON' //says it is JSON datatype
 
-			}).done(function(data){
-console.log(data.cats[0].photo)
+			}).done(function(data){ //runs when the request is successful
+
+
+	 let cats= data.cats
+
 // Creates a new <img> tag -> var element = document.createElement('img');
 
+for (let i=0 ;i < cats.length; i++){
+	console.log(cats[i])
 
-			let newcat = document.createElement("IMG"); //so we are creating a image element for the image
 
-			
-			let catbox = document.getElementById("cat1"); //so we are getting a picture of a cat
-			newcat.src = data.cats[0].photo; // so we are grabbing a picture from the api
-			newcat.alt="photo of " + data.cats[0].name; //so we are making its
-			//alt in the img descriptor show the cats name
-
+let catbox = document.getElementById("cat"+(i+1));
+//iterate through every cat in the API and insert its photo
+			let newcat = document.createElement("IMG"); //so we are creating a image element for the image.
+			 //so we are getting a space in the div to insert the picture
+			newcat.src = cats[i].photo; // says please add the image in the space ".
+			newcat.alt="photo of " + cats[i].name; //so we are making its alt in the img descriptor show the cats name
 
 			catbox.appendChild(newcat);
 // Inserts that <img> into one of the empty .cat-box divs'
-
-create a for loop to go thru the cats and add
+}
+// create a for loop to go thru the cats and add
 			})
 
 
